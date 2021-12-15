917886b117331c9e88c7178eaeb380dee3e84e79
MuzaffarBashir <mbas@ciklum.com>
2021-12-15 21:12:42 +0500

    Mocking db for getting list of todos fail scenario

--- src/tododb/dbTodo_test.go
+++ src/tododb/dbTodo_test.go
@@ -23,6 +23,7 @@ func NewMock() (*sql.DB, sqlmock.Sqlmock) {
 	return db, mock
 }
 
+//success case for get todos from db
 func TestGetAllToDO(t *testing.T) {
 	db, mock := NewMock()
 	todo := &Todo{
@@ -30,7 +31,7 @@ func TestGetAllToDO(t *testing.T) {
 	}
 	query := `SELECT id,description FROM "todolist"`
 	expectedRows := []string{"id", "description"}
-	expectedRS := sqlmock.NewRows(expectedRows).FromCSVString("1 New TODO")
+	expectedRS := sqlmock.NewRows(expectedRows).FromCSVString("1 new todo")
 	mock.ExpectQuery(query).WillReturnRows(expectedRS)
 	actualTodo, err := todo.GetAllToDO(todo.Connection)
 	totalActualRows := 0
@@ -40,4 +41,25 @@ func TestGetAllToDO(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotNil(t, actualTodo)
 	assert.EqualValues(t, 1, totalActualRows)
+}
+
+//Failure case for geting list of  todo
+func TestGetToDO_EmptyTodoList(t *testing.T) {
+	db, mock := NewMock()
+	todo := &Todo{
+		Connection: db,
+	}
+	defer db.Close()
+	query := `SELECT id,description FROM "todolist"`
+	expectedRows := []string{}
+	expectedRS := sqlmock.NewRows(expectedRows)
+	mock.ExpectQuery(query).WillReturnRows(expectedRS)
+	actualTodo, actualErr := todo.GetAllToDO(todo.Connection)
+	totalActualRows := 0
+	for actualTodo.Next() {
+		totalActualRows++
+	}
+	assert.Nil(t, actualErr)
+	assert.NotNil(t, actualTodo)
+	assert.Equal(t, len(expectedRows), totalActualRows)
 }
