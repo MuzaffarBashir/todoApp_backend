e6f5db5aa3a8d0e9004580fc47238fed36cc0b95
MuzaffarBashir <mbas@ciklum.com>
2021-12-16 00:19:16 +0500

    Pass test case for main.go to get list of todos

--- src/main.go
+++ src/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"database/sql"
 	"net/http"
+	"todoApp/tododb"
 	"todoApp/todoserver"
 )
 
 var Connection *sql.DB
 var servertodo *todoserver.ToDoServer
 
+func init() {
+	Connection = tododb.GetConnection()
+	servertodo = todoserver.NewServerTODO()
+}
 func getAllTodo(w http.ResponseWriter, r *http.Request) {
 	// calling to get list of todos
 	servertodo.GetAllToDo(w, Connection)
