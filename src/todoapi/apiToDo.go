8f5263805e4701b171ba9565f2c766bdd471b702
MuzaffarBashir <mbas@ciklum.com>
2021-12-15 17:13:27 +0500

    Adding some business logic to TODOAPI for gettinmg all todos

--- src/todoapi/apiToDo.go
+++ src/todoapi/apiToDo.go
@@ -1,1 +1,24 @@
 package todoapi
+
+import (
+	"database/sql"
+	"todoApp/tododb"
+)
+
+func (apiTodo *ApiToDo) GetAllToDo(conn *sql.DB) ([]ApiToDo, error) {
+
+	var todosList []ApiToDo
+	tododb := tododb.NewTodo()
+	data, err := tododb.GetToDO(conn)
+	if err != nil {
+		return nil, err
+	}
+	for data.Next() {
+		newtodo := NewApiService()
+		if err := data.Scan(&newtodo.ID, &newtodo.Description); err != nil {
+			panic(err)
+		}
+		todosList = append(todosList, *newtodo)
+	}
+	return todosList, nil
+}
